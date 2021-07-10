7515d4ed1ab835d01671cc70dc74760412a52e88
fxcvd <rzet8@mail.ru>
2021-07-10 17:27:59 +0300

    1.0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,35 @@
 from setuptools import setup
 
-with open("readme.md") as readme:
-    description = readme.read()
-    readme.close()
+description = """
+# SmsServiceOnline 
+`pip3 install SmsServiceOnline==1.0.1`
+
+## Example #1
+```python
+import SmsServiceOnline
+
+KEY = "<api key here>"
+LANG = "ru"
+MESSAGE = False
+
+
+api = SmsServiceOnline.Api(
+    api_key=KEY,
+    lang=LANG,
+    welcome_message=MESSAGE,
+)
+
+api.get_balance()
+```
+```python
+>>> 10.2
+```
+
+### all methods -> [here](https://sms-service-online.com/ru/api-sms-activate/)  
+
+---
+```by @fxcvd with ❤```
+"""
 
 setup(name='SmsServiceOnline',
     version='1.0.1',
